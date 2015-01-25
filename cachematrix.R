8ed704339e3453abf2f243c1702f43ae4b9b3534
marksalada <marksalada@yahoo.com>
2015-01-25 10:32:40 -0800

    completed cached inverse function updates

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function creates a chache-able matrix inverse function
+## and the second function is the matrix inverse function itself
 
-## Write a short comment describing this function
+## Returns a list of functions to enable caching of a matrix inverse
+##
+## 1) set the matrix
+## 2) get the matrix
+## 3) set the value of the matrix inverse
+## 4) get the value of the matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	invx <- NULL
+	set<-function(y){
+		x <<- y
+		invx <<- NULL
+	}
+	get<-function() x
+	setinv<-function(inv) invx <<- inv
+	getinv <- function() invx
+	list(set = set, get = get,
+		setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Calculates the inverse of a matrix if it is not already cached
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+	## check for bad entry
+	if(!is.list(x)){		
+		message("entry bad or not cacheable: use makeCacheMatrix() first")
+		return(NULL)
+	}
+	if(is.null(x[["getinv"]])){
+		message("missing method: use makeCacheMatrix() first")
+		return(NULL)
+	}
+
+	## grab and use if the inv is cached already
+	invx <- x$getinv()
+	if(!is.null(invx)){
+		message("getting cached data")
+		return(invx)
+	}
+
+	## inv wasn't cached, so grab matrix
+	data <- x$get()
+
+	## calculate inverse; this would be the place to 
+	## check if the matrix is invertable
+	## (e.g., abs(det(x)) > threshold)
+	invx <- solve(data)
+	
+	## cache the result
+	x$setinv(invx)
+
+     	## Return a matrix that is the inverse of 'x'
+	invx
 }
